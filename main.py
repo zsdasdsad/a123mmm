fd478e516df0e1fb786e7a998c7a85e652ea31d5
zsdasdsad <82306828+zsdasdsad@users.noreply.github.com>
2021-04-18 13:28:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ game = True
 
 ball = image.load("ppb.png")
 ball = transform.scale(ball,(50,50))
+p1 = image.load("sh.png")
+p2 = image.load("crow.png")
 
 bg = image.load("fon.jpg")
 while game:
     scr.blit(bg,(0,0))
     scr.blit(ball,(0,0))
+    scr.blit(p1,(100,100))
+    scr.blit(p2,(200,200))
     for e in event.get():
         if e.type == QUIT:
             game = False

74260fc88ed0f26a4702c5c267dcc886ca6b87d7
zsdasdsad <82306828+zsdasdsad@users.noreply.github.com>
2021-04-25 12:37:16 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ game = True
 ball = image.load("ppb.png")
 ball = transform.scale(ball,(50,50))
 p1 = image.load("sh.png")
+p1 = transform.scale(p1,(300,300))
 p2 = image.load("crow.png")
+p2 = transform.scale(p2,(100,200))
 
 bg = image.load("fon.jpg")
 while game:
     scr.blit(bg,(0,0))
     scr.blit(ball,(0,0))
-    scr.blit(p1,(100,100))
-    scr.blit(p2,(200,200))
+    scr.blit(p1,(-90,0))
+    scr.blit(p2,(1100,10))
     for e in event.get():
         if e.type == QUIT:
             game = False
